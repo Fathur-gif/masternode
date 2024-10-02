0cff971474a62d1ebe7a0219ab341f31ef81525f
Fathur-gif <fathuroke17@gmail.com>
2024-10-02 09:29:38 +0700

    menambahkan source code masternode platformIO

---
+++ source-code/Master_Mega/src/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+
+int soilMoisturePin_1 = A0;  
+int sensorValue_1 = 0;
+int soilMoisturePin_2 = A1;
+int sensorValue_2 = 0;
+int soilMoisturePin_3 = A2;
+int sensorValue_3 = 0;
+
+
+void setup() {
+  // Inisialisasi komunikasi serial
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Membaca nilai analog dari sensor kelembaban tanah
+  sensorValue_1 = analogRead(soilMoisturePin_1);
+  sensorValue_2 = analogRead(soilMoisturePin_2);
+  sensorValue_3 = analogRead(soilMoisturePin_3);
+
+  // Mengubah nilai ke rentang yang lebih sesuai, misalnya 0-100 persen
+  //int moisturePercent = map(sensorValue_1, 0, 4095, 0, 100);
+  //int moisturePercent_2 = map(sensorValue_2, 0, 4095,0,100);
+
+  // Tampilkan nilai kelembaban tanah ke serial monitor
+  Serial.print("Soil Moisture Level 1: ");
+  Serial.println(sensorValue_1);
+  //Serial.println("%");
+  Serial.print("Soil Moisture Level 2: ");
+  Serial.println(sensorValue_2);
+  //Serial.println("%");
+  Serial.print("Soil Moisture Level 3: ");
+  Serial.println(sensorValue_3);
+  //Serial.println("%");
+  Serial.println("-------------------------------");
+
+  // Tunggu 1 detik sebelum pembacaan berikutnya
+  delay(1000);
+}

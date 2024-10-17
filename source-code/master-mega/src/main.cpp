968f425c970aabf9727362e3b4870ef9501e2eb4
Fathur-gif <fathuroke17@gmail.com>
2024-10-17 10:26:46 +0700

    mengubah code master-mega menjadi hanya sensor rk520-01 dan mematikan fungsi lora

--- source-code/master-mega/src/main.cpp
+++ source-code/master-mega/src/main.cpp
@@ -1,17 +1,14 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <LoRa.h>
+//#include <LoRa.h>
 
 // Pin untuk sensor soil moisture dan suhu
-int soilMoisturePin_1 = A0;  // A0
-int soilMoisturePin_2 = A1;  // A1
-int soilMoisturePin_3 = A2;  // A2 (untuk RK520-01 Moisture)
-int soilTemperature = A3;    // A3 (untuk RK520-01 Temperature)
+int soilMoisturePin = A0;  // A0 (untuk RK520-01 Moisture)
+int soilTemperature = A1;    // A1 (untuk RK520-01 Temperature)
 
 int sensorValue_1 = 0;
 int sensorValue_2 = 0;
-int sensorValue_3 = 0;
-int sensorValue_4 = 0;
+
 
 void setup() {
   // Inisialisasi komunikasi serial
@@ -29,23 +26,15 @@ void setup() {
 
 void loop() {
   // Membaca nilai analog dari sensor kelembaban tanah dan suhu
-  sensorValue_1 = analogRead(soilMoisturePin_1);  // Sensor Moisture 1
-  sensorValue_2 = analogRead(soilMoisturePin_2);  // Sensor Moisture 2
-  sensorValue_3 = analogRead(soilMoisturePin_3);  // RK520-01 Moisture
-  sensorValue_4 = analogRead(soilTemperature);    // RK520-01 Temperature
+  sensorValue_1 = analogRead(soilMoisturePin);  // RK520-01 Moisture
+  sensorValue_2 = analogRead(soilTemperature);    // RK520-01 Temperature
 
   // Tampilkan nilai kelembaban tanah dan suhu ke serial monitor
-  Serial.print("Soil Moisture Level 1: ");
+  Serial.print("Soil Moisture Level: ");
   Serial.println(sensorValue_1);
-  
-  Serial.print("Soil Moisture Level 2: ");
-  Serial.println(sensorValue_2);
-
-  Serial.print("Soil Moisture Level 3: ");
-  Serial.println(sensorValue_3);
 
   Serial.print("Soil Temperature: ");
-  Serial.println(sensorValue_4);
+  Serial.println(sensorValue_2);
   Serial.println("-------------------------------");
 
   // Buat string data untuk dikirimkan melalui LoRa

b4297bf056bc5f60f39749a6b4552224f255a2e3
Fathur-gif <fathuroke17@gmail.com>
2024-10-21 12:47:43 +0700

    Menambahkan code lora dalam master-mega

--- source-code/master-mega/src/main.cpp
+++ source-code/master-mega/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <SPI.h>
-
+#include <LoRa.h>
 
 // Pin untuk sensor soil moisture dan suhu
 int soilMoisturePin = A0;  // A0 (untuk RK520-01 Moisture)
@@ -9,12 +9,30 @@ int soilTemperature = A1;  // A1 (untuk RK520-01 Temperature)
 int sensorValue_1 = 0;
 int sensorValue_2 = 0;
 
+// Define pin mapping
+#define ss 10         // NSS pin
+#define rst 7         // RST pin
+#define dio0 2        // DIO0 pin
+
 
 
 void setup() {
   // Initialize communication
   Serial.begin(9600);  // For monitoring
-  
+  while (!Serial);
+
+  Serial.println("LoRa Transmitter");
+
+  // Inisialisasi LoRa dengan pin yang ditentukan
+  LoRa.setPins(ss, rst, dio0);
+
+  // Set frequency sesuai kebutuhan, 920 MHz untuk Indonesia
+  if (!LoRa.begin(920E6)) {
+    Serial.println("Gagal memulai LoRa!");
+    while (1);
+  }
+  Serial.println("LoRa siap!");
+
 }
 
 void loop() {
@@ -31,7 +49,16 @@ void loop() {
   Serial.print("Soil Temperature: ");
   Serial.println(sensorValue_2);
 
-  
+  // Mengirim data sensor melalui LoRa
+  Serial.println("Mengirim data sensor melalui LoRa...");
+
+  // Mulai mengirim paket LoRa
+  LoRa.beginPacket();
+  LoRa.print("Soil Moisture: ");
+  LoRa.print(sensorValue_1);
+  LoRa.print(", Soil Temperature: ");
+  LoRa.print(sensorValue_2);
+  LoRa.endPacket();
 
   delay(1000);
 }

66a6a8fff928d82510b1b1a57960c87c211be9c8
Fathur-gif <fathuroke17@gmail.com>
2024-10-29 13:22:24 +0700

    code rs485 baru

--- source-code/master-mega/src/main.cpp
+++ source-code/master-mega/src/main.cpp
@@ -1,66 +1,60 @@
 #include <Arduino.h>
 #include <SPI.h>
+#include <SoftwareSerial.h>
 #include <ModbusMaster.h>
 
-// RS485 Communication setup
-#define MAX485_DE      8
-#define MAX485_RE_NEG  9
-#define RX1 18
-#define TX1 19
+#define RX1 19
+#define TX1 18 // RX, TX
+#define MAX485_DE 53
+#define MAX485_RE_NEG 52
 
 ModbusMaster node;
-
-uint8_t result;
-float temp, hum;
 void preTransmission()
 {
   digitalWrite(MAX485_RE_NEG, 1);
   digitalWrite(MAX485_DE, 1);
 }
-
 void postTransmission()
 {
   digitalWrite(MAX485_RE_NEG, 0);
   digitalWrite(MAX485_DE, 0);
 }
-
-void setup() {
-
+void setup()
+{
   pinMode(MAX485_RE_NEG, OUTPUT);
   pinMode(MAX485_DE, OUTPUT);
+  // Init in receive mode
   digitalWrite(MAX485_RE_NEG, 0);
   digitalWrite(MAX485_DE, 0);
-  // Initialize communication
-  Serial.begin(9600);  // For monitoring
-  Serial1.begin(9600); // RS485
+  Serial.println("start init serial 0");
+  Serial.begin(9600);
 
-  // RS485 direction control
+  while (!Serial) {
+  Serial.println("loop for init serial 0");
+  }
+  Serial.println("start init software serial");
+  Serial1.begin(9600);
 
-  // Modbus node setup
-  node.begin(1, Serial1);  // Modbus address 1, using Serial1 for RS485
+  while (!Serial1) {
+  Serial.println("loop for init software serial");
+  }
+  node.begin(1, Serial1);
   node.preTransmission(preTransmission);
   node.postTransmission(postTransmission);
-
-  Serial.println("Modbus communication initialized.");
 }
-
-void loop() {
+void loop()
+{
   uint8_t result;
-  uint16_t data;
-
-  result = node.readHoldingRegisters(0x0000, 1);
-
-
-  // Reading data from XY-MD02 over RS485
-  if (result == node.ku8MBSuccess) {
-    temp = node.getResponseBuffer(0) / 10.0f;
-    hum = node.getResponseBuffer(1) / 10.0f;
-    //    Serial.print("Temp: "); Serial.print(temp); Serial.print("\t");
-    //    Serial.print("Hum: "); Serial.print(hum);
+  uint16_t data[2];
+  Serial.println("get data");
+  result = node.readInputRegisters(1, 2);
+  if (result == node.ku8MBSuccess)
+  {
+    Serial.print("Temp: ");
+    Serial.println(node.getResponseBuffer(0)/10.0f);
+    Serial.print("Humi: ");
+    Serial.println(node.getResponseBuffer(1)/10.0f);
     Serial.println();
-    node.clearResponseBuffer();
-    node.clearTransmitBuffer();
   }
-
-  delay(1000);
+ delay(1000);
 }
